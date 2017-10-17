4f42adbc9812190af69458eb854eef17150d81ce
Kon-NB <Kon-NB@DESKTOP-5MGOQA6>
2017-10-17 23:13:25 +0700

    Week09P06

--- Week09/Week09/Form1.cs
+++ Week09/Week09/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,7 +21,7 @@ namespace Week09
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            Brush mybrush = new SolidBrush(Color.DarkGreen);
+            HatchBrush mybrush = new HatchBrush(HatchStyle.DarkVertical, Color.White, Color.Violet);
             Pen mypen = new Pen(mybrush, 5);
             g.DrawEllipse(mypen, 10, 10, 200, 200);
             mypen.Dispose();
---
+++ Week09/Week09/HatchBrush.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace Week09
+{
+    internal class HatchBrush
+    {
+        private object s;
+        private Color violet;
+
+        public HatchBrush(System.Drawing.Drawing2D.HatchStyle darkVertical, object s)
+        {
+            this.s = s;
+        }
+
+        public HatchBrush(HatchStyle darkVertical, object s, Color violet) : this(darkVertical, s)
+        {
+            this.violet = violet;
+        }
+    }
+}
