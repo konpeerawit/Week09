79ba47c1db97e796f1950f115f690508db201444
Kon-NB <Kon-NB@DESKTOP-5MGOQA6>
2017-10-22 15:06:54 +0700

    Week09P13

--- Week09/Week09/Form1.cs
+++ Week09/Week09/Form1.cs
@@ -22,13 +22,13 @@ namespace Week09
         {
             Graphics g = e.Graphics;
             Pen penEllipse = new Pen(Color.Brown);
-            penEllipse.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+            penEllipse.DashStyle = DashStyle.Dash;
             g.DrawEllipse(penEllipse, 20, 20, 200, 150);
-
-            Pen penArc = new Pen(Color.Magenta, 2);
-            g.DrawArc(penArc, 20, 20, 200, 150, 90, 180);
+            Pen penPie = new Pen(Color.Magenta, 2);
+            g.DrawPie(penPie, 20, 20, 200, 150, 45, 90);
+            Pen penPie1 = new Pen(Color.BlueViolet, 2);
+            g.DrawPie(penPie, 20, 20, 200, 150, 150, 45);
             g.Dispose();
-
 
         }
     }
