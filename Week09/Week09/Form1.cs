3a12473743cfe790a7d4fdd967f7324aa6b70870
Kon-NB <Kon-NB@DESKTOP-5MGOQA6>
2017-10-22 17:33:00 +0700

    Week09P19

--- Week09/Week09/Form1.cs
+++ Week09/Week09/Form1.cs
@@ -11,13 +11,24 @@ using System.Windows.Forms;
 
 namespace Week09
 {
-    private void Form1_Paint(object sender, PaintEventArgs e)
+    public partial class Form1 : Form
     {
-        Graphics g = e.Graphics;
-        Bitmap bmp = new Bitmap("D:\\59030194.jpg");
-        this.SetClientSizeCore(bmp.Width + 20, bmp.Height + 20);
-        g.DrawImage(bmp, 10, 10);
-        g.Dispose();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Bitmap bmp = new Bitmap("D:\\59030194.Png");
+            Rectangle destrect = new Rectangle(10, 10, bmp.Width / 2, bmp.Height);
+            Rectangle srcrect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            this.SetClientSizeCore(destrect.Width + 20, destrect.Height + 20);
+            g.DrawImage(bmp, destrect, srcrect, GraphicsUnit.Pixel);
+            g.Dispose();
+
+        }
+
     }
-    
 }
